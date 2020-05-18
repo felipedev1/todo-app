dd2b0dbf987e22eca5a75b5ec65c76cbe8b577bb
Felipe <felipe.pereira.dev@gmail.com>
2020-05-18 15:28:34 -0300

    components Todo and About

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,15 @@
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'font-awesome/css/font-awesome.min.css'
 import React from 'react';
 import './App.css';
+import Todo from './todo/Todo'
+import About from './about/About'
 
 function App() {
   return (
-    <div>
-      Init
+    <div className="container">
+      <Todo />
+      <About />
     </div>
   );
 }
---
+++ frontend/src/todo/Todo.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Todo() {
+  return (
+    <div>
+      <h1>TODO</h1>
+    </div>
+  )
+}
