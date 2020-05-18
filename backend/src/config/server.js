c1a17f707e982f7c215f861a1afd31193d3be608
Felipe <felipe.pereira.dev@gmail.com>
2020-05-18 14:29:13 -0300

    adding the cors manually

--- backend/src/config/server.js
+++ backend/src/config/server.js
@@ -1,10 +1,13 @@
-const port = 3003
 const bodyParser = require('body-parser')
 const express = require('express')
 const server = express()
+const allowCors = require('./cors')
+
+const port = 3003
 
 server.use(bodyParser.urlencoded({extended: true}))
 server.use(bodyParser.json())
+server.use(allowCors)
 
 server.listen(port, () => console.log('BACKEND is running on port', port))
 
