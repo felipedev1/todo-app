188e61974d5f80f8e30e8b4ebb16d74ff94ab307
Felipe <felipe.pereira.dev@gmail.com>
2020-05-19 12:17:44 -0300

    add event with axios/ method post

--- frontend/src/todo/Todo.js
+++ frontend/src/todo/Todo.js
@@ -1,13 +1,17 @@
+import axios from 'axios'
 import React, {useState} from 'react'
 import PageHeader from '../template/PageHeader'
 import TodoForm from './TodoForm'
+
+const URL = 'http://localhost:3003/api/todos'
 
 export default function Todo() {
   const [description, setDescription] = useState('')
   const [list, setList] = useState([])
 
   const handleAdd = () => {
-    console.log(description)
+    axios.post(URL, { description })
+      .then(res => console.log(res.data))
   }
 
   const handleChange = (e) => {
