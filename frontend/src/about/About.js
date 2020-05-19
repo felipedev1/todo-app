e0d8e8fab1d3416fdff688e0c5b7d9831f7b09e5
Felipe <felipe.pereira.dev@gmail.com>
2020-05-19 10:36:29 -0300

    PageHeader component

--- frontend/src/about/About.js
+++ frontend/src/about/About.js
@@ -1,9 +1,16 @@
 import React from 'react'
+import PageHeader from '../template/PageHeader'
 
 export default function About() {
   return (
     <div>
-      <h1>About</h1>
+      <PageHeader name="Sobre" small="Nós" />
+      <h2>Nossa História</h2>
+      <p>Lorem ipsum dolor sit amet...</p>
+      <h2>Missão e Visão</h2>
+      <p>Lorem ipsum dolor sit amet...</p>
+      <h2>Imprensa</h2>
+      <p>Lorem ipsum dolor sit amet...</p>
     </div>
   )
 }
--- frontend/src/todo/Todo.js
+++ frontend/src/todo/Todo.js
@@ -1,9 +1,10 @@
 import React from 'react'
+import PageHeader from '../template/PageHeader'
 
 export default function Todo() {
   return (
     <div>
-      <h1>TODO</h1>
+      <PageHeader name="Tarefas" small="Cadastro" />
     </div>
   )
 }
